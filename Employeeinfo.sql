98aea086ea2527a53a80651839ffa6143de13033
saimonazad <saimonazad@azad>
2016-07-14 00:46:55 +0600

    12:46PM 14 july 2016

---
+++ Employeeinfo.sql
@@ -0,0 +1,3 @@
+
+CREATE TABLE "Employeeinfo" ("employeeid" INTEGER PRIMARY KEY  NOT NULL  DEFAULT (1) ,"name" CHAR,"surname" CHAR,"age" INTEGER,"password" CHAR, "username" CHAR);
+INSERT INTO "Employeeinfo" VALUES(1,'saimon','azad',22,'root','root');
---
+++ src/MainFrame/javaconnect.java
@@ -0,0 +1,38 @@
+package MainFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author saimonazad
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.*;
+
+
+public class javaconnect {
+Connection conn=null;
+public static Connection ConnecrDb()
+{
+    try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection conn=DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=SuperShop;user=root;password=root");
+        return conn;
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Connection Established");
+        return null; 
+    }
+}
+    public javaconnect() {
+        
+    }
+    
+    
+    
+}
